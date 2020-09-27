a02ad19be62de17bb2ecd5da7d0a09795a0967ce
Kirill <arkhipov.km@gmail.com>
2020-09-27 15:59:00 +0200

    moved certain utility functions to musify package

--- main.go
+++ main.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strconv"
 	"strings"
 
@@ -17,25 +15,6 @@ import (
 	"github.com/arkhipovkm/musify/utils"
 	"github.com/arkhipovkm/musify/vk"
 )
-
-func httpGET(uri string) ([]byte, error) {
-	resp, err := http.Get(uri)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
-}
-
-func httpGETChan(uri string, dataChan chan []byte, errChan chan error) {
-	data, err := httpGET(uri)
-	dataChan <- data
-	errChan <- err
-}
 
 func escapeWindowsPath(s string) string {
 	forbidden := []string{
@@ -62,27 +41,20 @@ func downloadAudioChan(i int, audio *vk.Audio, album *vk.Playlist, dirname strin
 		return
 	}
 	base := fmt.Sprintf("%02d", i+1) + " — " + escapeWindowsPath(audio.Performer) + " — " + escapeWindowsPath(audio.Title)
-	if len([]rune(dirname))+len([]rune(base)) > 200 {
-		base = string([]rune(base)[:200-len([]rune(dirname))])
+	dirnameRune := []rune(dirname)
+	baseRune := []rune(base)
+	if len(dirnameRune)+len(baseRune) > 200 {
+		base = string(baseRune)[:200-len(dirnameRune)]
 	}
 	filename := filepath.Join(
 		dirname,
 		base+".mp3",
 	)
-	// Download audio file
-	if strings.Contains(audio.URL, ".m3u8") {
-		re := regexp.MustCompile("/[0-9a-f]+(/audios)?/([0-9a-f]+)/index.m3u8")
-		audio.URL = re.ReplaceAllString(audio.URL, "$1/$2.mp3")
-		_, _, err = download.MP3File(audio.URL, filename)
-	} else if strings.Contains(audio.URL, ".mp3") {
-		_, _, err = download.MP3File(audio.URL, filename)
-	} else {
-		err = fmt.Errorf("Unsupported file type: %s", filepath.Base(filepath.Dir(audio.URL)))
+	err = download.Download(audio, filename)
+	if err != nil {
 		errChan <- err
 		return
 	}
-	// End Download audio file
-
 	// Handle trck tag
 	var trck string
 	if album != nil {
@@ -113,53 +85,6 @@ func downloadAudioChan(i int, audio *vk.Audio, album *vk.Playlist, dirname strin
 	return
 }
 
-func downloadApics(audio *vk.Audio, album *vk.Playlist) ([]byte, []byte) {
-	// Handle APICs tags: cover and icon
-	apicErrChan := make(chan error, 2)
-	apicDataChan := make(chan []byte, 2)
-	var apicCoverData, apicIconData []byte
-
-	var apicCover string
-	if album != nil && album.CoverURL != "" {
-		apicCover = album.CoverURL
-	} else {
-		apicCover = audio.CoverURLp
-	}
-
-	if audio.CoverURLp != "" {
-		go httpGETChan(apicCover, apicDataChan, apicErrChan)
-	} else {
-		apicErrChan <- nil
-		apicDataChan <- nil
-	}
-	if audio.CoverURLs != "" {
-		go httpGETChan(audio.CoverURLs, apicDataChan, apicErrChan)
-	} else {
-		apicErrChan <- nil
-		apicDataChan <- nil
-	}
-
-	for i := 0; i < 2; i++ {
-		err := <-apicErrChan
-		if err != nil {
-			log.Println("Error loading APICs: ", err)
-		}
-	}
-
-	apic0 := <-apicDataChan
-	apic1 := <-apicDataChan
-
-	if len(apic0) < len(apic1) {
-		apicCoverData = apic1
-		apicIconData = apic0
-	} else {
-		apicCoverData = apic0
-		apicIconData = apic1
-	}
-	// End Handle APICs tags: cover and icon
-	return apicCoverData, apicIconData
-}
-
 func main() {
 	vkUser := vk.NewDefaultUser()
 	err := vkUser.Authenticate()
@@ -185,7 +110,10 @@ func main() {
 	}
 	for _, albumID := range albumIDs {
 		playlist := vk.LoadPlaylist(albumID, vkUser)
-
+		if playlist == nil {
+			log.Println("Nil Playlist. Continuing..")
+			continue
+		}
 		errChan := make(chan error, len(playlist.List))
 		dirname := filepath.Join(
 			"D:",
@@ -209,7 +137,7 @@ func main() {
 				panic(err)
 			}
 			playlist.DecypherURLs(vkUser)
-			apicCoverData, apicIconData := downloadApics(playlist.List[0], playlist)
+			apicCoverData, apicIconData := download.DownloadAPICs(playlist.List[0], playlist)
 			log.Println("Downloaded APICs: ", len(apicCoverData), len(apicIconData))
 			for i, audio := range playlist.List {
 				go downloadAudioChan(i, audio, playlist, dirname, apicCoverData, apicIconData, errChan)
