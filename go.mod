bf01ee0383809db29e80cb80a5fa2d7e06c34729
Kirill <arkhipov.km@gmail.com>
2021-09-16 23:56:19 +0200

    init branch

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/arkhipovkm/musify-downloader
+
+go 1.17
+
+require (
+	github.com/arkhipovkm/id3-go v0.0.0-20210827221627-680f3dc12ff2
+	github.com/arkhipovkm/musify v0.0.0-20210828235114-ee374c9f4b6a
+)
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/grafov/m3u8 v0.11.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20200501053045-e0ff5e5a1de5 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,143 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
+	"time"
 
-	"github.com/arkhipovkm/id3-go"
 	"github.com/arkhipovkm/musify/download"
-	"github.com/arkhipovkm/musify/utils"
-	"github.com/arkhipovkm/musify/vk"
 )
 
-var BASE_PATH string = ""
-
-func escapeWindowsPath(s string) string {
-	forbidden := []string{
-		"<",  // (less than)
-		">",  // (greater than)
-		":",  // (colon - sometimes works, but is actually NTFS Alternate Data Streams)
-		"\"", // (double quote)
-		"/",  // (forward slash)
-		"\\", // (backslash)
-		"|",  // (vertical bar or pipe)
-		"?",  // (question mark)
-		"*",  // (asterisk)
-	}
-	for _, char := range forbidden {
-		s = strings.ReplaceAll(s, char, "_")
-	}
-	return s
-}
-
-func downloadAudio(i int, audio *vk.Audio, album *vk.Playlist, dirname string, apicCoverData, apicIconData []byte) error {
-	var err error
-	if audio.URL == "" {
-		return err
-	}
-	base := fmt.Sprintf("%02d", i+1) + " — " + escapeWindowsPath(audio.Performer) + " — " + escapeWindowsPath(audio.Title)
-	dirnameRune := []rune(dirname)
-	baseRune := []rune(base)
-	if len(dirnameRune)+len(baseRune) > 200 {
-		base = string(baseRune)[:200-len(dirnameRune)]
-	}
-	filename := filepath.Join(
-		dirname,
-		base+".mp3",
-	)
-	err = download.Download(audio, filename)
-	if err != nil {
-		return err
-	}
-	// Handle trck tag
-	var trck string
-	if album != nil {
-		trck = strconv.Itoa(i+1) + "/" + strconv.Itoa(album.TotalCount)
-	}
-	// End Handle trck tag
-
-	// Write ID3 tags to file
-	id3File, err := id3.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		err = id3File.Close()
-		if err != nil {
-			log.Println(err)
-		}
-	}()
-	utils.SetID3TagAPICs(id3File, apicCoverData, apicIconData)
-	utils.SetID3Tag(
-		id3File,
-		album.AuthorName, // audio.Performer,
-		audio.Title,
-		album.Title,
-		album.YearInfoStr,
-		trck,
-	)
-	// Write ID3 tags to file
-
-	return err
-}
-
 func main() {
-
-	playlistsFilePath := os.Args[1]
-	BASE_PATH = os.Args[2]
-
-	vkUser := vk.NewDefaultUser()
-	err := vkUser.Authenticate("", "")
-	if err != nil {
-		log.Printf("%#v\n", vkUser)
-		panic(err)
-	}
-	body, err := ioutil.ReadFile(playlistsFilePath)
-	if err != nil {
-		panic(err)
-	}
-	var failedAlbums []string
-	for _, albumID := range strings.Split(string(body), "\r\n") {
-		if albumID[0] == '#' {
-			continue
-		}
-		playlist := vk.LoadPlaylist(albumID, vkUser)
-		if playlist == nil {
-			log.Println("Nil Playlist. Continuing..")
-			continue
-		}
-		dirname := filepath.Join(
-			BASE_PATH,
-			escapeWindowsPath(playlist.AuthorName),
-			escapeWindowsPath(playlist.Title),
-		)
-		log.Println("Starting download ", dirname)
-		err = os.MkdirAll(
-			dirname,
-			os.ModePerm,
+	for {
+		t1 := time.Now()
+		_, n, err := download.MP3File(
+			// "https://psv4.vkuseraudio.net/audio/ee/bu_xR9d1CEKU214FiRfs5aSyK1IXjAd8-WsePA/a6Ojw4OTExPw/ddQ09LOFhTa2o-PVw/index.m3u8?extra=JpX3heC1rRR8scoEwp_2nuTRPGKmRbjxGuLfW33F-i1tD7_9_WQxsZ1-757aKAtJJnUldk5XewTbedmb2aBNl8P9_2xyOmciseBVzlWPfFPJP3K2nfI6Mne0rdo-2sHIPK55uQ-_QnU5Law6og",
+			"https://psv4.vkuseraudio.net/c6187/u190985821/audios/a80d8cf8c3d1.mp3?extra=jTqp0A3ly2T6hxynbuvIDMpWYDL2NY74vzsVzwSftH6A5TQLkPfecuL1sClbTSv09IB0ApSCuQJK6Y2bKGarEzZ4naXoSBKSlQC7wtwvgibMLof4JkGP4B4F-5RpxZau76R4lKLbe0KPiEXDru3v-A&long_chunk=1",
+			"test.mp3",
 		)
 		if err != nil {
 			panic(err)
 		}
-		playlist.AcquireURLs(vkUser)
-		playlist.DecypherURLs(vkUser)
-		playlist.CoverURL = ""
-		apicCoverData, apicIconData := download.DownloadAPICs(playlist.List[0], playlist)
-		log.Println("Downloaded APICs: ", len(apicCoverData), len(apicIconData))
-		for i, audio := range playlist.List {
-			err = downloadAudio(i, audio, playlist, dirname, apicCoverData, apicIconData)
-			if err != nil {
-				log.Println(err)
-				failedAlbums = append(failedAlbums, albumID)
-				break
-			}
-		}
-		log.Println("Finished download ", dirname)
+		t2 := time.Now()
+		log.Printf("Fetched audio: %d bytes in %.1f ms, %.1f MB/s\n", n, float64(t2.UnixNano()-t1.UnixNano())/float64(1e6), float64(n)/float64(1e6)/(float64(t2.UnixNano()-t1.UnixNano())/float64(1e9)))
 	}
-	log.Println(failedAlbums)
 }
